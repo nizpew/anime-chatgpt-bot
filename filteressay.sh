cd03e3c997cd6acf677b05791579b53c1d0b6dbc
nizpew <profissionalArb@proton.me>
2025-02-26 08:36:27 -0400

    windows support

--- filteressay.sh
+++ filteressay.sh
@@ -9,5 +9,5 @@
 
 
 
-tgpt -q "I will give you a prompt. reponse in less than 50 words, Roleplay as an anime shoujo (anime genre, search its meaning) character. search their quotes if needed. now here is the prompt - $1" | grep -o '"[^"]*"'
+tgpt --provider duckduckgo  -q -w "I will give you a prompt. reponse in less than 50 words, Roleplay as an anime shoujo (anime genre, search its meaning) character. search their quotes if needed. now here is the prompt - $1" 
 
---
+++ readme.txt
@@ -0,0 +1,18 @@
+##WINDOWS
+
+
+cd ~/anime-chatgpt-bot
+python -m venv nome_do_ambiente
+
+source venv/bin/activate
+
+
+pip install flask
+python main.py
+
+
+start firefox http://127.0.0.1:5000
+
+
+
+
